051acc700c888fcf184089171eeb90c3baa37454
regis066 <mugisharegis72@gmail.com>
2025-01-06 21:12:13 +0200

    Add category item card component

--- lib/widgets/category_item_card.dart
+++ lib/widgets/category_item_card.dart
@@ -1,3 +1,4 @@
+
 import "dart:ffi";
 import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';
@@ -24,25 +25,25 @@ class CategoryItemCard extends StatelessWidget {
                 end: Alignment.bottomRight,
                 colors: [Color(0xFF353F54), Color(0xFF222834)]),
             borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.4),
-            blurRadius: 10,
-            offset: Offset(0, 8)
-          )
-        ]),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.4),
+                  blurRadius: 10,
+                  offset: Offset(0, 8)
+              )
+            ]),
         child: Center(
           child: svgPath != null
               ? SvgPicture.asset(svgPath!, width: 30, height: 30)
               : (text != null
-                  ? Text(
-                      text!,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )
-                  : SizedBox.shrink()),
+              ? Text(
+            text!,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          )
+              : SizedBox.shrink()),
         ),
       ),
     );
